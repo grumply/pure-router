3f619079d9ed70afb12edbf67c055d10892f73b8
grumply <sean@grump.ly>
2018-05-30 00:03:42 -0500

    Split off pure-uri.

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, ef, excelsior, pure-core, pure-default, pure-events, pure-html, pure-lifted, pure-txt, stdenv
+{ mkDerivation, base, ef, excelsior, pure-core, pure-default, pure-events, pure-html, pure-lifted, pure-txt, pure-uri, stdenv
 }:
 mkDerivation {
   pname = "pure-router";
   version = "0.7.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base ef excelsior pure-core pure-default pure-events pure-html pure-lifted pure-txt ];
+  libraryHaskellDepends = [ base ef excelsior pure-core pure-default pure-events pure-html pure-lifted pure-txt pure-uri ];
   homepage = "github.com/grumply/pure-router";
   license = stdenv.lib.licenses.bsd3;
 }
