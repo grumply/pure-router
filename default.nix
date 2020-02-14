07066ef5348c4b0828cabad03fb2f817ee3013be
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:21 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 }:
 mkDerivation {
   pname = "pure-router";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base ef excelsior pure-core pure-default pure-events pure-html pure-lifted pure-txt pure-uri ];
   homepage = "github.com/grumply/pure-router";
